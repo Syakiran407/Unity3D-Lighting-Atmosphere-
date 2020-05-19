424666dab78baa7475ffaf2f29322aa5516f6dab
MAHMUD SYAKIRAN <1821054@chester.ac.uk>
2020-05-20 00:12:54 +0800

    Add logic script to the sphere and display debug message on console

---
+++ Assets/SphereLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereLogic : MonoBehaviour
+{
+
+    int m_framecounter = 0;
+
+
+    // Start is called before the first frame update
+    void Start() //void start is executed once the game starts before the first frame update
+    {
+        Debug.Log("Hello World, this is a sphere"); //Print debug message on Console
+    }
+
+    // Update is called once per frame
+    void Update() //Is called once per frame because when the game runs 60 fps per second, this function will called 60 times per second
+    {
+        ++m_framecounter; 
+
+        Debug.Log("Number of frames passed: " + m_framecounter);
+    }
+}
