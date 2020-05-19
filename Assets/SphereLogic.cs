2ee6d5aa78d5cf56437c3e7ddb7e087810d01533
MAHMUD SYAKIRAN <1821054@chester.ac.uk>
2020-05-20 00:27:56 +0800

    Create a trigger to add force to the sphere (Jumping)

--- Assets/SphereLogic.cs
+++ Assets/SphereLogic.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class SphereLogic : MonoBehaviour
 {
 
-    int m_framecounter = 0;
 
+    Rigidbody m_rigidbody; //Access the rigidbody
 
     // Start is called before the first frame update
     void Start() //void start is executed once the game starts before the first frame update
     {
-        Debug.Log("Hello World, this is a sphere"); //Print debug message on Console
+        m_rigidbody = GetComponent<Rigidbody>(); //Search all of the components of an object type RigidBody and store in the variable
     }
 
     // Update is called once per frame
     void Update() //Is called once per frame because when the game runs 60 fps per second, this function will called 60 times per second
     {
-        ++m_framecounter; 
-
-        Debug.Log("Number of frames passed: " + m_framecounter);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_rigidbody.AddForce(new Vector3(0, 1000, 0));
+        }
     }
 }
